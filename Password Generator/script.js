f7582960d3e8b5354f4461deec9c049b1c9fa1c0
Cristina Ruiz <cristinaruiz96@yahoo.com>
2020-09-24 21:02:53 -0500

    Re-organized the layout of the code

--- Password Generator/script.js
+++ Password Generator/script.js
@@ -1,14 +1,3 @@
-// Assignment Code
-//When generate button is clicked, there will be a pop up box asking to confirm new password
-var generateBtn = document.querySelector("#generate");
-var upperCaseLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-var lowerCaseLetters= ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var numbers = ["1","2","3","4","5","6","7","8","9","0"];
-var special =["!","@","#","$","%","^","&","*","(",")","_","+"];
-var possibleTypes =[];
-var result = "";
-//var possibleTypes= upperCaseLetters.concat(lowerCaseLetters) (do this for each scenario)
-
 function generatePassword(){
 
   //These are all of the criteria questions for the new password
@@ -17,6 +6,16 @@ function generatePassword(){
  var upperCase = confirm("Would you like to include upper case letters?");
  var numeric = confirm("Would you like to include numeric values?");
  var special = confirm("Would you like to include special characters?");
+  var password = "";
+
+  //Password criteria values
+var upperCaseLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var lowerCaseLetters= ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var numbers = ["1","2","3","4","5","6","7","8","9","0"];
+var special =["!","@","#","$","%","^","&","*","(",")","_","+"];
+var possibleTypes ="";
+//var possibleTypes= upperCaseLetters.concat(lowerCaseLetters) (do this for each scenario)
+
 
  if ((parseInt(length) >=8 && parseInt(length) <= 128) && (lowerCase || upperCase || numeric || special)) {
    //If these criteria are confirmed, they will be added to the password var which is possible types
@@ -36,28 +35,20 @@ function generatePassword(){
   //For loop to generate which characters will be chosen at random
   for (var i=0; i < length; i++){
     var random = Math.floor(Math.random() * possibleTypes.length)
-    result = result + possibleTypes.parseInt(random);
+    password = password + possibleTypes.parseInt(random);
   }
-  return result;
+  return password;
  }
   
 
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+//Button connection to HTML
+var generateBtn = document.querySelector("#generate");
 
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
- 
-  passwordText.value = password;
-  
-
-
-
-}
-
-
-
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+  passwordText.value = password;}
